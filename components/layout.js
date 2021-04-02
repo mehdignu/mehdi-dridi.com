2ac81789c8f04353b6c574bc3d09837a24a5d919
Mehdi <dridignu@gmail.com>
2021-04-02 23:40:48 +0200

    style the portfolio

---
+++ components/layout.js
@@ -0,0 +1,65 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+const name = 'Mehdi Dridi'
+export const siteTitle = 'Home | Mehdi Dridi'
+
+export default function Layout({ children, home }) {
+  return (
+    <>
+    <div className={styles.line}></div>
+
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;1,600&display=swap" rel="stylesheet" />
+        <meta
+          name="description"
+          content="Mehdi Dridi. I'm a software engineer obsessed with code quality, software architecture, and building experiences."
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+        
+      </Head>
+
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpeg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+           
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+    </>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hi, i'm Mehdi. i'm a Software engineer based in Berlin, Germany</p>
+
+        <p>I write code. All kinds, but mostly JavaScript, Python and PHP. I love pie and I have opinions. Especially about JavaScript and pie.</p>
+
+        <p>I strive for clean, readable, performance optimized code.</p>
+
+        <p>You can find my resume <a href="files/resume.pdf" target="_blank">here</a>. ■</p>
+      </section>
+     
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+
+      <ul className={utilStyles.mainList}>
+
+        <li><Link href="/posts">Writing</Link></li>
+        <li><a href="https://github.com/mehdignu" target="_blank">GitHub</a></li>
+        <li><a href="https://twitter.com/simplyianm">Twitter</a></li>
+        <li><a href="https://twitter.com/simplyianm">Contact</a></li>
+      
+      </ul>
+
+      </section>
+    </Layout>
+  )
+}
